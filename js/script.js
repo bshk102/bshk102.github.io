698c7745f6589d4ec7c1a1ea00512f203cd1a2b0
Your God <ass@hole>
2020-12-16 17:39:19 +0500

    know

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 let clientwidth = document.documentElement.clientWidth
 const img = document.querySelector('.main__img')
 const line = document.querySelector('.for__line')
+const line2 = document.querySelector('.know__line')
 const snake = document.querySelector('.for__snake')
+const snake2 = document.querySelector('.know__snake')
 
 if (clientwidth > 1440) {
     img.style.right = (clientwidth - 1440) / 2 + 'px'
     line.style.left = (clientwidth - 1440) / 2 + 'px'
+    line2.style.right = (clientwidth - 1440) / 2 + 'px'
     snake.style.left = 186 + (clientwidth - 1440) / 2 + 'px'
+    snake2.style.left = 106 + (clientwidth - 1440) / 2 + 'px'
 }
 
 console.log(document.documentElement.clientHeight)
