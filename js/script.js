5c8b0687875792f0777e67c7a6026aa948d5f62d
Your God <ass@hole>
2020-12-17 17:29:36 +0500

    start adaptive

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const line3 = document.querySelector('.about__line')
 const snake = document.querySelector('.for__snake')
 const snake2 = document.querySelector('.know__snake')
 const snake3 = document.querySelector('.gifts__snake')
+const lines = document.querySelectorAll('.line')
 
 if (clientwidth > 1440) {
     img.style.right = (clientwidth - 1440) / 2 + 'px'
@@ -15,4 +16,13 @@ if (clientwidth > 1440) {
     snake.style.left = 186 + (clientwidth - 1440) / 2 + 'px'
     snake2.style.left = 106 + (clientwidth - 1440) / 2 + 'px'
     snake3.style.left = 300 + (clientwidth - 1440) / 2 + 'px'
+}
+
+if (clientwidth < 1440) {
+    line.style.width = 181 - (1440 - clientwidth) / 2 + 'px'
+    line2.style.width = 181 - (1440 - clientwidth) / 2 + 'px'
+    line3.style.width = 181 - (1440 - clientwidth) / 2 + 'px'
+    snake.style.left = 186 - (1440 - clientwidth) / 2 + 'px'
+    snake2.style.left = 106 - (1440 - clientwidth) / 2 + 'px'
+    snake3.style.left = 300 - (1440 - clientwidth) / 2 + 'px'
 }
