14089113beefab4c1971550c0d0546f88939228c
Your God <ass@hole>
2020-12-17 16:02:59 +0500

    footer

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const line2 = document.querySelector('.know__line')
 const line3 = document.querySelector('.about__line')
 const snake = document.querySelector('.for__snake')
 const snake2 = document.querySelector('.know__snake')
+const snake3 = document.querySelector('.gifts__snake')
 
 if (clientwidth > 1440) {
     img.style.right = (clientwidth - 1440) / 2 + 'px'
@@ -13,7 +14,5 @@ if (clientwidth > 1440) {
     line3.style.left = (clientwidth - 1440) / 2 + 'px'
     snake.style.left = 186 + (clientwidth - 1440) / 2 + 'px'
     snake2.style.left = 106 + (clientwidth - 1440) / 2 + 'px'
+    snake3.style.left = 300 + (clientwidth - 1440) / 2 + 'px'
 }
-
-console.log(document.documentElement.clientHeight)
-console.log(document.documentElement.clientWidth)
