99d1563fa8d624c65ba7275acf2838fcb4f24b07
Your God <ass@hole>
2021-01-12 17:44:09 +0500

    start phpmailer

--- sendmail.php
+++ sendmail.php
@@ -17,4 +17,30 @@
     //Тема письма
     $mail->Subject = 'Hello';
 
-    
+    //Тело письма
+    $body = '<h1>H1</h1>';
+
+    if (trim(!empty($_POST['name']))) {
+        $body.='<p><strong>Имя:</strong> '$_POST['name'].'</p>';
+    }
+
+    if (trim(!empty($_POST['tel']))) {
+        $body.='<p><strong>Телефон:</strong> '$_POST['tel'].'</p>';
+    }
+
+    if (trim(!empty($_POST['email']))) {
+        $body.='<p><strong>Email:</strong> '$_POST['email'].'</p>';
+    }
+
+    //Отправляем
+    if (!$mail->send()) {
+        $message = 'Ошибка';
+    } else {
+        $message = 'Данные отправлены';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
